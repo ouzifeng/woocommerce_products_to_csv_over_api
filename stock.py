a11bdb86c7df0d0a8447648dffcdb92a7d748cfb
Ouzifeng <mr.davidoak@gmail.com>
2023-09-14 12:10:23 +0100

    Update stock.py
    Hide API keys

--- stock.py
+++ stock.py
@@ -2,11 +2,11 @@ import requests
 import csv
 
 def fetch_all_products():
-    base_url = "https://thetackletavern.co.uk/wp-json/wc/v3/"
+    base_url = "ADD YOUR STORE URL/wp-json/wc/v3/"
     product_url = base_url + "products"
     params = {
-        "consumer_key": "ck_033584debafe468c9b441298a89c21a4d499cf3e",
-        "consumer_secret": "cs_84b7b046fbeda0095b20f84e122c2dce2ca7a5d3",
+        "consumer_key": "ADD API KEY",
+        "consumer_secret": "ADD API SECRET",
         "per_page": 100,
         "page": 1
     }
@@ -30,7 +30,7 @@ def fetch_all_products():
                 sku = product['sku']
                 price = product['price']
                 stock_quantity = product['stock_quantity']
-                all_products.append(["The Tackle Tavern", "en", sku, price, stock_quantity])
+                all_products.append([sku, price, stock_quantity])
             elif product['type'] == 'variable':
                 # Fetch variations for this product
                 variations_url = product_url + f"/{product['id']}/variations"
@@ -39,7 +39,7 @@ def fetch_all_products():
                     sku = variation['sku']
                     price = variation['price']
                     stock_quantity = variation['stock_quantity']
-                    all_products.append(["The Tackle Tavern", "en", sku, price, stock_quantity])
+                    all_products.append([sku, price, stock_quantity])
         
             batch_counter += 1
             if batch_counter % batch_size == 0:
@@ -54,7 +54,7 @@ def fetch_all_products():
     print("Writing to CSV...")
     with open('woocommerce_products.csv', 'w', newline='') as file:
         writer = csv.writer(file)
-        writer.writerow(['Vendor', 'Language', 'Product Code', 'Price', 'Quantity'])
+        writer.writerow(['Product Code', 'Price', 'Quantity'])
         writer.writerows(all_products)
     
     print("CSV generation complete.")
